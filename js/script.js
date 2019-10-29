744a5927b8f28bfacf55cf772525b96fbd33c5d1
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-30 00:13:29 +0100

    finish exercise without function , but is finish !

--- js/script.js
+++ js/script.js
@@ -1,67 +1,68 @@
-/* NOME REPO:  js-campominato
-DESCRIZIONE:
- Il computer deve generare 16 numeri casuali da 1 a 100.
-In seguito deve chiedere all’utente di inserire un numero da 1 a 100 alla volta,
-se il numero è presente nella lista dei numeri generati, la partita termina,
-altrimenti continua chiedendo all’utente un altro numero.
-La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-Ci sono 2 controlli che van fatti già di base per dar senso all’ex:
-i 16 numeri vietati/mina, devono essere tutti diversi, non possono esserci doppioni;
-l’utente non può inserire due volte lo stesso numero, ma sempre numeri diversi.
- */
-//@ts-check
+// Il computer deve generare 16 numeri casuali da 1 a 100.
+// In seguito deve chiedere all’utente di inserire un numero da 1 a
+// 100 alla volta, se il numero è presente nella lista dei numeri
+// generati, la partita termina, altrimenti continua chiedendo
+// all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero
+// “vietato” o raggiunge il numero massimo possibile di numeri
+// consentiti.
+// Al termine della partita il software deve comunicare il punteggio,
+//   cioè il numero di volte che l’utente ha inserito un numero
+// consentito.
 
-//Il computer deve generare 16 numeri casuali da 1 a 100.
-function numRandom(min, max) {
+//genera un numero casuale fra 1 e max
+var gruppoMine = [];
+var numRand;
+
+while(gruppoMine.length<16){
+    numRand = getRandomInt(1,101);
+    if(gruppoMine.includes(numRand) == false){
+        gruppoMine.push(numRand);
+    }
+}
+console.log("i numeri della morte sono : " + gruppoMine)
+
+var beccato = false;
+var gruppoNumeriPlayer = [];
+var sceltaPlayer;
+var punteggio = 0;
+
+while (beccato == false){
+    sceltaPlayer = parseInt(prompt("Scegli un numero tra 1 e 100"));
+    console.log("Il numero scelto dall'utente è :" + sceltaPlayer)
+    if(sceltaPlayer > 0 && sceltaPlayer < 101 && beccato == false && !isNaN(sceltaPlayer)){
+        if (gruppoNumeriPlayer.includes(sceltaPlayer) == true){
+            alert("Non fare il furbo , hai già inserito questo numero")
+        }else if(gruppoMine.includes(sceltaPlayer) == true){
+            alert("La mina è esplosa sei morto");
+            beccato = true;
+        }else{
+            alert("l'hai mancata per un soffio , puoi continuare...per ora")
+            gruppoNumeriPlayer.push(sceltaPlayer);
+            punteggio++;
+        }
+    }else{
+        alert("inserisci un valore valido")
+    }
+
+}
+
+if(beccato==true){
+    console.log("sei morto facendo " + punteggio + "punti")
+}
+
+if(punteggio == 84 && beccato == false){
+    console.log("sembra impossibile , ma hai vinto... Porta simone in tabaccheria")
+}
+
+//Blocco funzioni
+
+function getRandomInt(min,max){
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //Il max è escluso e il min è incluso
+    return Math.floor(Math.random()* (max-min)) + min; // il max è escluso il min è incluso
 }
 
-//creo una variabile che sarà uguale alla mia funzione per generare i numeri casuali
-var numRandomUser = numRandom(1,101);
-console.log(numRandomUser)
 
-//creo un array dove inserirò i numeri casuali generati (NON DOPPI)
-var numVietati = [];
-console.log(numVietati.length);
-var i = 0;
-var numGen;
 
-//imposto un ciclo while per far generare 16 numeri diversi e allo stesso tempo faccio fare un controllo tramite la funzione includes che mi permette di verificare se all'interno dell'array sono già presenti i numeri generati casualmente -->
-while (numVietati.length < 16){
-     numGen = numRandom(1,101);
 
-     //<-- se il comando includes mi restituisce falso , allora lo pusho all'interno del mio array numeri generati
-    if(numVietati.includes(numGen) == false){
-        numVietati.push(numGen);
-    }
-    console.log(numVietati.length);
-}
-
-console.log(numVietati)
-
-//In seguito deve chiedere all’utente di inserire un numero da 1 a 100 alla volta,se il numero è presente nella lista dei numeri generati, la partita termina,
-//altrimenti continua chiedendo all’utente un altro numero.
-
-var numGiocatore;
-var gruppoNumeriGiocatore = [];
-
-while (gruppoNumeriGiocatore.length < 84 && gruppoNumeriGiocatore == numVietati){
-    numGiocatore = parseInt(prompt("inserisci un numero"));
-    gruppoNumeriGiocatore.push(numGiocatore);
-
-    if (gruppoNumeriGiocatore == numVietati){
-        console.log("mi dispiace hai perso")
-    }else{
-        console.log("complimenti hai vinto")
-    }
-
-    /* if(gruppoNumeriGiocatore.includes(numGiocatore)==false){
-        gruppoNumeriGiocatore.push(numGiocatore);
-    }else(
-        alert("attenzione hai già inserito questo numero")
-    ) */
-}
-console.log(gruppoNumeriGiocatore)
