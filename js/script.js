198c42850a48b8494b04f8e69fea9e8a24797de4
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-29 13:31:14 +0100

    create "loop for" for generate 16 random number from 1 to 100

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ Ci sono 2 controlli che van fatti già di base per dar senso all’ex:
 i 16 numeri vietati/mina, devono essere tutti diversi, non possono esserci doppioni;
 l’utente non può inserire due volte lo stesso numero, ma sempre numeri diversi.
  */
+
+//Il computer deve generare 16 numeri casuali da 1 a 100.
+var numeriPc ; 
+
+for(i=0; i<16 ; i++){
+    numeriPc = Math.floor(Math.random() * 100 + 1);
+    console.log(numeriPc)
+}
+
+
+//In seguito deve chiedere all’utente di inserire un numero da 1 a 100 alla volta
